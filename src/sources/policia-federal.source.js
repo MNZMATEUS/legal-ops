765d2d196a9bfd32b4481769747090e77041dc22
Mateus M. Menezes <112094820+MNZMATEUS@users.noreply.github.com>
2026-02-10 19:56:41 -0300

    Delete src directory

--- src/sources/policia-federal.source.js
+++
@@ -1,6 +0,0 @@
-const BaseSource = require('./base-source');
-
-class PoliciaFederalSource extends BaseSource {
-}
-
-module.exports = PoliciaFederalSource;
--- src/sources/receita-federal.source.js
+++
@@ -1,6 +0,0 @@
-const BaseSource = require('./base-source');
-
-class ReceitaFederalSource extends BaseSource {
-}
-
-module.exports = ReceitaFederalSource;
